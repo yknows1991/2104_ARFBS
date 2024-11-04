33c241b6fd67c6b0c8b60801a7b5acdfc8879c2a
Inocencio Madriaga <inocenciomadriaga@yKnows-MacBook-Pro.local>
2024-11-04 08:23:55 +0800

    Project template

---
+++ MyProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myproject;
+
+/**
+ *
+ * @author inocenciomadriaga
+ */
+public class MyProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
